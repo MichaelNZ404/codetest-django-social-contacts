dcea3063c489a024651cd288b7bdc32fd65d9e54
Michael Coleman <michael.coleman404@gmail.com>
2017-12-22 21:43:26 +0000

    displaying names

--- rolepointsite/social/views.py
+++ rolepointsite/social/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from django.template import loader
 
 def index(request):
+    search_string = request.GET.get('search', '')
     data = json.load(open('exercise-data.json'))
-    context = {'results': data}
+    names = [x['name'] for x in data if search_string in x['name']]
+    context = {'results': names}
     return render(request, 'social/index.html', context)
